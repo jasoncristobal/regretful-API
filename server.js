d82c2b1881efef87b8089a5b69abc5aebe3fbe3b
Jason Cristobal <jason@jasoncristobal.com>
2018-10-25 15:51:01 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+const cors = require('cors');
+const { CLIENT_ORIGIN } = require('./config');
+
+app.use(
+    cors({
+        origin: CLIENT_ORIGIN
+    })
+);
+
+app.get('/api/*', (req, res) => {
+    res.json({ ok: true });
+});
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+
+module.exports = { app };
